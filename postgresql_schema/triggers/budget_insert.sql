76fe8ae0420e9272fbd4e028db32daddf57c247c
Nathan Barlow <nathanjbarlow@gmail.com>
2023-04-20 22:15:00 -0500

    Fix budget insert that was inserting for transactions

--- postgresql_schema/triggers/budget_insert.sql
+++ postgresql_schema/triggers/budget_insert.sql
@@ -4,5 +4,5 @@ CREATE FUNCTION budget_insert() RETURNS trigger AS $budget_insert$
     END;
 $budget_insert$ LANGUAGE plpgsql;
 
-CREATE TRIGGER budget_insert AFTER INSERT ON transaction
+CREATE TRIGGER budget_insert AFTER INSERT ON budget
     FOR EACH ROW EXECUTE FUNCTION budget_insert();
